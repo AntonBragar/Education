730d0929c4f203969cc5381b94fc2dff7c8744b2
AntonBragar <anton.bragar@gmail.com>
2020-11-12 12:08:24 +0200

    11/12/2020 Anton`s Commit

---
+++ Math/math.js
@@ -0,0 +1,29 @@
+/*
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}*/
+
+/*let a = 2;
+let x = 2 + (a *= 2)
+console.log(a,x)*/
+
+/*
+let a = Number(prompt("Первое число?", 1));
+let b = Number(prompt("Второе число?", 2));
+
+console.log(a + b); // 12
+
+*/
+
+
+/*
+let result = 5 && null && 10
+console.log(result)
+*/
+
+let age = 15;
+if ( 14 <= age <= 90) {
+    console.log("nice")
+}
+
+
